e3e0ea2fe885b7f2838f3f7640657f5cc23bfeba
sparshstpl <sparshstpl@gmail.com>
2021-03-19 13:51:53 +0530

    remove field from application table

---
+++ prisma/migrations/20210319082121_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `unitApplicationId` on the `UnitApplication` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UnitApplication" DROP COLUMN "unitApplicationId";
